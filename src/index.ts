63178cd8ca97f475f0980a12577f063305055a2d
janefawkes <devilcemeterykitten@gmail.com>
2022-07-18 13:53:42 +0300

    formatting + cookies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 require('dotenv').config({ path: '../env.d.ts' });
+var cors = require('cors')
 
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants"
@@ -9,23 +10,55 @@ import { buildSchema } from "type-graphql"
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
     const orm = await MikroORM.init(config);    
     await orm.getMigrator().up;
-    
+
     const app = express()
+
+    const RedisStore = connectRedis(session);
+    const redis = new Redis(process.env.REDIS_URL);
+
+    // await redisClient.connect()
+
+    app.use(
+        cors({
+          credentials: true,
+          origin: "https://studio.apollographql.com",
+        }),
+        session({
+          name: "qid",
+          store: new RedisStore({ 
+            client: redis,
+            disableTouch: true
+          }),
+          cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+            httpOnly: true,
+            secure: __prod__,
+            sameSite: 'none'
+          },
+          saveUninitialized: false,
+          secret: "whatever",
+          resave: false,
+        })
+    )
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        context: () => ({ em: orm.em })
+        context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
     })
     
     await apolloServer.start();
-    apolloServer.applyMiddleware({ app })
+    apolloServer.applyMiddleware({ app, cors: false })
 
     app.listen(4000, () => {
         console.log("server started on localhost:4000")
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 import { MyContext } from "../types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver, ObjectType } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, Resolver, ObjectType, Query } from "type-graphql";
 import argon2 from 'argon2'
 
 @InputType()
@@ -14,65 +14,106 @@ class UserDataInput {
 @ObjectType()
 class FieldError {
     @Field()
-    field: string
+    field: string;
     @Field()
-    message: string
+    message: string;
 }
 
 @ObjectType()
 class UserResponse {
-    @Field(() => [FieldError], {nullable: true})
-    errors?: FieldError[]
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
 
-    @Field(() => User, {nullable: true})
-    user?: User
+    @Field(() => User, { nullable: true })
+    user?: User;
 }
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Query(() => User, { nullable: true })
+    async me(@Ctx() { req, em }: MyContext) {
+        if (!req.session.userId) {
+            return null
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId })
+        return user
+    }
+    @Mutation(() => UserResponse)
     async register(
         @Arg("options", () => UserDataInput) options: UserDataInput,
         @Ctx() { em }: MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [{
+                    field: 'username',
+                    message: 'length must be greater than 2'
+                },
+                ]
+            }
+        }
+        if (options.password.length <= 2) {
+            return {
+                errors: [{
+                    field: 'password',
+                    message: 'length must be greater than 2'
+                },
+                ]
+            }
+        }
         const hashedPassword = await argon2.hash(options.password)
         const user = em.create(User, {
             username: options.username,
             password: hashedPassword
         })
-    await em.persistAndFlush(user)
-    return user
+
+        try {
+            await em.persistAndFlush(user)
+        } catch (err) {
+            if (err.code == "23505") {
+                return {
+                    errors: [{
+                        field: "username",
+                        message: "this username has already been taken"
+                    }]
+                }
+            }
+        }
+
+        return { user }
     }
-    
+
     @Mutation(() => UserResponse)
     async login(
-        @Arg("options", () => UserDataInput) options: UserDataInput,
-        @Ctx() { em }: MyContext
+        @Arg("options") options: UserDataInput,
+        @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
-        const user = await em.findOne(User, {username: options.username})        
+        const user = await em.findOne(User, { username: options.username })
         if (!user) {
             return {
                 errors: [
                     {
-                    field: "username",
-                    message: "that username doesn't exist",
+                        field: "username",
+                        message: "that user doesn't exist",
                     },
                 ],
             }
         }
-    const valid = await argon2.verify(user.password, options.password)
-    if (!valid) {
+        const valid = await argon2.verify(user.password, options.password)
+        if (!valid) {
             return {
                 errors: [
                     {
-                    field: "password",
-                    message: "invalid password",
+                        field: "password",
+                        message: "invalid password",
                     },
                 ],
             }
-    }
-    return {
-        user
-    }
+        }
+
+        req.session.userId = user.id
+
+        return { user }
     }
 }
